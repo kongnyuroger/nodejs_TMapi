da232534de8d0d508358f892f0adbe95a8e6f8e3
Kongnyu Roger <kongnyuroger711@gmail.com>
2025-08-29 21:06:57 +0100

    set default environmental variables

--- src/database/db.js
+++ src/database/db.js
@@ -3,10 +3,10 @@ import 'dotenv/config'
 import { Pool } from "pg";
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
+    user: process.env.DB_USER  || 'roger',
+    host: process.env.DB_HOST || 'localhost',
+    database: process.env.DB_NAME || 'tmapi_db',
+    password: process.env.DB_PASSWORD ,
     port: process.env.DB_PORT,
 })
 
